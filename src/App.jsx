21394af6c77b635508d78aa0eca0a68b79cb6fde
hugorcsar-rgb <hugorcsar@gmail.com>
2026-05-25 20:39:00 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { Routes, Route, useLocation } from 'react-router-dom'
-import { useEffect } from 'react'
-import Layout from './components/Layout.jsx'
-import Home from './pages/Home/index.jsx'
+// src/App.jsx
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import Layout from './components/Layout.jsx';
+import Home from './pages/Home/index.jsx';
+import Founders from './pages/Founders.jsx';
+import Recruit from './pages/Recruit.jsx';
+import Consult from './pages/Consult.jsx';
+import Camps from './pages/Camps.jsx';
 
-// Scrolls to top of page when navigating between routes
 function ScrollToTop() {
-  const { pathname } = useLocation()
-  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
-  return null
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
 }
 
 export default function App() {
@@ -17,16 +23,15 @@ export default function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
-          {/* Add more routes here as we build each page:
-              <Route path="/founders" element={<Founders />} />
-              <Route path="/recruit"  element={<Recruit />} />
-              <Route path="/consult"  element={<Consult />} />
-              <Route path="/camps"    element={<Camps />} />
-              <Route path="/partners" element={<Partners />} />
-              <Route path="/inquire"  element={<Inquire />} />
-          */}
+          <Route path="/founders" element={<Founders />} />
+          <Route path="/recruit" element={<Recruit />} />
+          <Route path="/consult" element={<Consult />} />
+          <Route path="/camps" element={<Camps />} />
+          {/* Coming next: */}
+          {/* <Route path="/partners" element={<Partners />} /> */}
+          {/* <Route path="/inquire" element={<Inquire />} /> */}
         </Route>
       </Routes>
     </>
-  )
+  );
 }
