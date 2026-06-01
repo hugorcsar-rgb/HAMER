8e9cbdbb172cff71559b2109204fec0d67c147d5
hugorcsar-rgb <hugorcsar@gmail.com>
2026-06-01 15:02:21 +0200

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import Nav from './Nav.jsx'
 import Footer from './Footer.jsx'
+import CustomCursor from './CustomCursor.jsx'
 
 export default function Layout() {
   return (
-    <div className="min-h-screen flex flex-col bg-background text-foreground">
+    <div className="relative min-h-screen flex flex-col bg-background text-foreground nazari-texture">
+      <CustomCursor />
       <Nav />
       <main className="flex-1">
         <Outlet />
